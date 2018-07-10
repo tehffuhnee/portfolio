ec737337faa8bb3b82f7127257ad3c11217ea0ae
tehffuhnee <41089388+tehffuhnee@users.noreply.github.com>
2018-07-10 16:37:48 -0600

    Create appleaegi.js

---
+++ appleaegi.js
@@ -0,0 +1,8 @@
+alert("Ready for some apples? ^_^");
+var cultivar = prompt("What's your favorite type of apple? owo");
+function showImage() {
+  var show = document.getElementById("show");
+  show.style.display = 'none';
+  var pic = document.getElementById("applePic");
+  pic.style.display = 'block';
+}
